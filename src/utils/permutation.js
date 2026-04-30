f8f33b4693d846b6a1c5db090e0df5fe378bc124
Starry <starsmate@gmail.com>
2026-04-30 14:17:38 -0400

    Fixed bugs in invertPerm and then implemented orderofPerm, permAreEqual and cycleNotation

--- src/utils/permutation.js
+++ src/utils/permutation.js
@@ -10,23 +10,57 @@ function composePerm(p1 , p2){
 function invertPerm(p){
     const result = [0,0,0,0]
     for (let x = 0; x < 4; x ++){
-        result[p[x]] = 1
+        result[p[x]] = x
     }
     return result
 }
 
 
 function orderofPerm(p){
-
+    const curr = [...p]
+    const iden = [0, 1, 2, 3]
+    const count = 1 
+    while (!permAreEqual(curr, iden)){
+        curr = composePerm(curr, p)
+        count++
+        if (count > 24){
+            break
+        }
+    } 
+    return count 
 }
 
 function permAreEqual(p1, p2){
-
+    return p1[0]==p2[0] && p1[1]==p2[1] && p1[2]==p2[2] && p1[3]==p2[3]
 }
 
 
 function cycleNotation(p){
+    const visit = [false, false, false, false]
+    const cycles = []
+    for (let x = 0; x < 4; x ++){
+        if (visit[x]){
+            continue
+        }
+        const cycle = []
+        const y = x 
+        while (!visit[y]){
+            visit[y] = true 
+            cycle.push(y + 1)
+            y = p[y]
+        }
 
+        if (cycle.length > 1){
+            cycles.push("(" + cycle.join(" ") + ")")
+
+        }
+    }
+
+    if (cycles.length == 0){
+        return 
+    }
+
+    return cycles.join("")
 
 }
 
@@ -37,5 +71,5 @@ function generateS4(){
 
 function randoTargetPerm(){
 
-    
+
 }
